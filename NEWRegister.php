25697b7918c2122297eea7bbb9f2e05c172ebed2
saicharan3 <charankatikala369@gmail.com>
2016-06-21 15:37:47 +0530

    Create NEWRegister.php

---
+++ NEWRegister.php
@@ -0,0 +1,16 @@
+<?php
+    $con = mysqli_connect("mysql40.000webhost.com", "a4794786_BUNNY", "Bhargavi321", "a4794786_OUFEED");
+    
+    $Email = $_POST["Email"];
+    $Password = $_POST["Password"];
+    $Username = $_POST["Username"];
+    $RPassword = $_POST["RPassword"];
+    $statement = mysqli_prepare($con, "INSERT INTO user (Email, Username, Password, RPassword) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $Email, $Username, $Password, $RPassword);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
